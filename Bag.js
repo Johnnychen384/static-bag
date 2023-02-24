f3e6966c5a35fcf22ee8e118994588d197d280af
Johnny Chen <Johnnychen384@gmail.com>
2023-02-24 14:01:45 -0500

    finished

--- Bag.js
+++ Bag.js
@@ -1,8 +1,11 @@
 class Bag {
+    static maxWeight = 23;
     constructor(weight) {
         if(!weight){
             throw new Error('bag needs weight')
         } 
+
+        if(weight > Bag.maxWeight) throw new Error('weight greater than maximum.')
         this.weight = weight
     }
 }
--- Bag.test.js
+++ Bag.test.js
@@ -3,11 +3,17 @@ const Bag = require('./Bag')
 describe('Bag class', () => {
     //bag has weight
     test('bag has weight', () => {
-        const bag = new Bag(25)
-        expect(bag.weight).toBe(25)
+        
+        expect(() => new Bag(25)).toThrowError("weight greater than maximum.")
     })
     //if bag has no weight, return error
     test('bag has no weight to return an error', () => {
         expect(() => new Bag()).toThrowError('bag needs weight')
+    })
+
+    // if bag weight exceeds maximum
+    test('bag does not exceed max weight', () => {
+
+        expect(() => new Bag(50)).toThrowError('weight greater than maximum.')
     })
 })
